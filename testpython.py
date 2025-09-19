70013d7fe36c6cdb42559d42218619cf704b706e
ujala <ujkaa001@osloskolen.no>
2025-09-19 09:32:47 +0200

    coden-temprature

--- testpython.py
+++ testpython.py
@@ -1,3 +1,26 @@
-print("Hello world!")
+#print("Hello world!")
 #Det er første filen
 #hello
+
+
+#Bonus-oppgave
+import os
+import random
+
+def get_temprature():
+
+temp_str = os.popen("vcgencmd measure_temp").readline()
+
+if "temp=" in temp_str:
+    temp = temp_str.replace("temp=", "").replace("'C\n","") 
+    return float(temp)
+else
+    return random.randint(15, 35)
+
+temprature = get_temprature()
+print("Temprature is:", temprature, "°C")
+
+if temprature > 25:
+    print("Its a hot day!")
+else:
+    print("Its a cool day.")
