e267e224a0e43cb4f5e7953a657ad6eb86041130
ujala <ujkaa001@osloskolen.no>
2025-09-19 10:45:06 +0200

    Pustil-Systeminformasjon

--- testpython.py
+++ testpython.py
@@ -4,17 +4,39 @@
 
 
 #Bonus-oppgave
-import random
+#Tempratur
+#import random
 
-def get_temperature():
+#def get_temperature():
    
-    return random.randint(15, 35)
+    #return random.randint(15, 35)
 
 
-temperature = get_temperature()
-print("Temperature is:", temperature, "°C")
+#temperature = get_temperature()
+#print("Temperature is:", temperature, "°C")
 
-if temperature > 25:
-    print("It's a hot day!")
+#if temperature > 25:
+    #print("It's a hot day!")
+#else:
+    #print("It's a cool day.")
+
+
+#Pustil-Systeminformasjon
+import pustil
+import time
+
+temps = pustil.sensors_tempratures()
+
+if tems:
+    for name, entries in temps.items():
+        for entry in entries:
+            print(f"{name} - {entry.label or 'CPU'}: {entry.current}°C")
+
 else:
-    print("It's a cool day.")
+    print("Tempraturdata ikke tilgjengelig på dette systemet.")
+
+
+uptime_seconds = time.time() - psutil.boot_time()
+uptime_hours = int(uptime_seconds // 3600)
+uptime_minutes = int((uptime_seconds % 3600) // 60)
+print("System har vært oppe i:", uptime_hours,"timer og", uptime_minutes, "minutter")
