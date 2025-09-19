c37c1a28f8e8f063240fc72b3719f29eacacd900
ujala <ujkaa001@osloskolen.no>
2025-09-19 09:52:29 +0200

    Tempraturen-code2

--- testpython.py
+++ testpython.py
@@ -4,23 +4,17 @@
 
 
 #Bonus-oppgave
-import os
 import random
 
-def get_temprature():
-
-temp_str = os.popen("vcgencmd measure_temp").readline()
-
-if "temp=" in temp_str:
-    temp = temp_str.replace("temp=", "").replace("'C\n","") 
-    return float(temp)
-else
+def get_temperature():
+   
     return random.randint(15, 35)
 
-temprature = get_temprature()
-print("Temprature is:", temprature, "°C")
 
-if temprature > 25:
-    print("Its a hot day!")
+temperature = get_temperature()
+print("Temperature is:", temperature, "°C")
+
+if temperature > 25:
+    print("It's a hot day!")
 else:
-    print("Its a cool day.")
+    print("It's a cool day.")
